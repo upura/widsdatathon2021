7db02596f6ce6bc69fb77d58508d82c435254898
upura <upura0@gmail.com>
2021-02-13 09:56:16 +0900

    weight005

--- experiments/weighted_averaging.py
+++ experiments/weighted_averaging.py
@@ -49,12 +49,14 @@ def make_submission(pred, run_name: str):
 
 
 run_ids = [
-    'run005',
-    'run006',
+    # 'run005',
+    # 'run006',
     'run007',
     'run008',
+    'run012',
+    # 'run013'
 ]
-run_name = 'weight004'
+run_name = 'weight005'
 
 if __name__ == '__main__':
     y_train = Data.load('../input/pickle/y_train_fe000.pkl')
